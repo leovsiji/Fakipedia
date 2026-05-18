148dbc9205d977d4909daf460572961539bc0bf9
leovsiji2025-joyhat <leovsiji2025@gmail.com>
2026-05-19 02:26:13 +0530

    added a database sqlite3 basically offline mode

--- fakipedia/faki/fakipedia.py
+++ fakipedia/faki/fakipedia.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup as b
 import requests as r
 import random
 import textwrap as tw
+import sqlite3 as sq
 
 
 def beyond():
@@ -23,18 +24,19 @@ def beyond():
         "  I♦♦♦♦♦♦♦♦♦♦♦√ .♦♦♦♦♦♦♦♦♦∇",             
         "  ⋆∇i     ≤♦♦♦√ .♦♦♦♫              mm        ##                               mm     ##",                   
         "          ≤♦♦♦√ .♦♦♦♫             ##                                         ##        ",                   
-        "          ≤♦♦♦√ .♦♦ m#####m  ##  ##      ####     ##m###m    m####m    m###m##   ####   m#####m",                   
-        '          ≤♦♦♦√ .♦♦♦  mmm##  ##m##        ##     ##"  "##  ##mmmm##  ##"  "##     ##      mmm##',                   
-        '          ≤♦♦♦√ .♦ m##"""##  ##"##m      ##     ##    ##  ##""""""  ##    ##     ##    m##"""##',                   
-        '          ≤♦♦♦√ .♦ ##mmm###  ## "#m  mmm##mmm  ###mm##"  "##mmmm#  "##mm###  mmm##mmm  ##mmm###',                   
-        '          ≤♦♦♦≥ -♦♦ """" ""  ""   "" """""""  ## """      """""     """"""  """"""""   """""""',                   
+        "          ≤♦♦♦√ .♦ m#####m  ##  ##      ####     ##m###m    m####m    m###m##   ####   m#####m",                   
+        '          ≤♦♦♦√ .♦♦  mmm##  ##m##        ##     ##"  "##  ##mmmm##  ##"  "##     ##      mmm##',                   
+        '          ≤♦♦♦√ . m##"""##  ##"##m      ##     ##    ##  ##""""""  ##    ##     ##    m##"""##',                   
+        '          ≤♦♦♦√ . ##mmm###  ## "#m  mmm##mmm  ###mm##"  "##mmmm#  "##mm###  mmm##mmm  ##mmm###',                   
+        '          ≤♦♦♦≥ -♦ """" ""  ""   "" """""""  ## """      """""     """"""  """"""""   """""""',                   
         "          ≤♦♦◦..•♦♦◦l                        ##  ",                   
         " Il.      ∂♦•;+◦♦♦≥",                     
         "♦♦♦♦♦⋆+  +♦√-•♦⋆÷",                       
         "♦♦♦♦♦♦♦♦♦♦°√>.",                          
         "  i±∂∫-: "                          
         "                                                    ",
-        "                        online(wiki extraction) - /on         exit - /e"                                        
+        "                        online(wiki extraction) - /on    offline storage - /of  ",
+        "                        exit - /e                                               ",                                        
                                             
         ]
         print()
@@ -42,6 +44,16 @@ def beyond():
             print(i)
         print()
 
+
+    con = sq.connect('lib.db')
+    c = con.cursor()
+    c.execute(""" 
+CREATE TABLE IF NOT EXISTS wikidata(
+              id INTEGER PRIMARY KEY AUTOINCREMENT,
+              heading TEXT,
+              content TEXT 
+              )
+""")
 
     def extract():
         file_name = " "
@@ -60,13 +72,12 @@ def beyond():
                     print()
                     print(f"--- {hed} ---")
                     break
-            file_name = hed +".txt"
-            with open(file_name ,"w")as f:
-                f.write(hed+'\n')
+            c.execute("INSERT INTO wikidata (heading) VALUES(?)",(hed,))
 
 
         url = input("LINK:- ")
         if url in "/e":
+            print("さよなら")
             exit()
         else:
             y =[]
@@ -83,10 +94,8 @@ def beyond():
                     print()
                     print(f"--- {hed} ---")
                     break
-            file_name = hed +".txt"
-            with open(file_name ,"w")as f:
-                f.write(hed+'\n')
-
+            c.execute("INSERT INTO wikidata (heading) VALUES(?)",(hed,))
+            
         
         headers = {'user-agent':random.choice(h)}
         re = r.get(url,headers=headers)
@@ -99,18 +108,95 @@ def beyond():
         #print(string)
         clean = tw.fill(string,width=100)
         print(clean)
-        with open(file_name,"a",encoding='utf-8-sig')as f:
+        aid = c.lastrowid
+        with open("temp.txt","a",encoding='utf-8-sig')as f:
             f.write('\n'+clean)
+        with open("temp.txt","r+",encoding='utf-8-sig')as f:
+            st = f.read()
+            c.execute("UPDATE wikidata SET content =? WHERE id =?",(st,aid))
+            con.commit()
+            f.truncate(0)
+        
+    def off(ofin):
+        c.execute("SELECT COUNT(*) FROM wikidata")
+        lel = c.fetchone()[0]
+        if "/help" in ofin:
+            print("""
+/li - lists the topics available in offline library syntax - /li 
+/fi - find the topics in library syntax - /fi topic
+/e - exit syntax - /e
+""")    
+            
+        elif "/li" in ofin:
+            if lel == 0:
+                print("library is empty")
+            elif lel > 0 : 
+                c.execute("select heading FROM wikidata")
+                rows = c.fetchall()
+                print()
+                for i, row in enumerate(rows, start=1):
+                    print(f"{i} - {row[0]} ")
+            
+        elif "/fi " in ofin:
+            re = ofin.split(" ")
+            del re[0]
+            rem = " ".join(re).capitalize()
+            c.execute("select * FROM wikidata WHERE heading = ?",(rem,))
+            rows = c.fetchall()
+            for row in rows:
+                print()
+                print(f"--- {row[1]} ---")
+                print(row[2])
+        elif "/del" in ofin:
+            de = ofin.split(" ")
+            if lel == 0:
+                print("library is empty")
+            elif lel > 0:
+                if len(de) == 1:
+                    dis = input(f"confirm delete all content by typing y to exit n --> ")
+                    if dis == "y":
+                        c.execute("DELETE FROM wikidata")
+                        con.commit()
+                        print("library deleted")
+                    else:
+                        print("さよなら")
+                        exit()
+                else:
+                    del de[0]
+                    dell = " ".join(de).capitalize()
+                    dis = input(f"confirm delete of {dell} by typing y to exit n --> ")
+                    if dis == "y":
+                        c.execute("DELETE FROM wikidata WHERE heading = ?",(dell,))
+                        con.commit()
+                        print(f"{dell} deleted")
+                    else:
+                        print("さよなら")
+                        exit()    
 
     banner()
     while True:
         print()
         dec = input("input:- ")
         if dec in "/e,e,E":
+            print("さよなら")
             exit()
         elif dec == "/on":
-            print()
-            extract()
+            while True:
+                print()
+                extract()
+        elif dec == "/of":
+            while True:
+                print()
+                ofin = input("図書館:- ")
+                if ofin in "/e,e,E":
+                    print()
+                    print("さよなら")
+                    exit()
+                else:
+                    off(ofin)
+                    
+            
+    con.close()
 beyond()
     
         
--- fakipedia/faki/t.py
+++
@@ -1,9 +0,0 @@
-import textwrap as tw
-
-with open("Nuclear power.txt","r",encoding='utf-8-sig')as f:
-    raw = f.read()
-
-clean = tw.fill(raw,width=70)
-
-with open("Nuclear power.txt","w",encoding='utf-8-sig')as f:
-    f.write(clean) 
